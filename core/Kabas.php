14b84581d4be405d67d1b893d5bc0ae46ed9c239
Adrien Leloup <contact@void.graphics>
2016-03-10 16:03:01 +0100

    Fixed indentation

--- core/Kabas.php
+++ core/Kabas.php
@@ -4,38 +4,38 @@ namespace Kabas;
 
 class Kabas
 {
-    /**
-     * The current Kabas version
-     *
-     * @var string
-     */
-    const VERSION = '0.0.1';
+      /**
+      * The current Kabas version
+      *
+      * @var string
+      */
+      const VERSION = '0.0.1';
 
-    /**
-     * Instanciate the application
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-          $this->Config = new Config\Container();
-    }
+      /**
+      * Instanciate the application
+      *
+      * @return void
+      */
+      public function __construct()
+      {
+            $this->config = new Config\Container();
+      }
 
-    public function boot()
-    {
-    $this->Request = new Http\Request();
-    $this->Router = new Http\Router();
-   }
+      public function boot()
+      {
+            $this->request = new Http\Request();
+            $this->router = new Http\Router();
+      }
 
 
-    /**
-     * Get the version number of this Kabas website.
-     *
-     * @return string
-     */
-    public function version()
-    {
-        return static::VERSION;
-    }
+      /**
+      * Get the version number of this Kabas website.
+      *
+      * @return string
+      */
+      public function version()
+      {
+            return static::VERSION;
+      }
 
 }
