5aef398b78c94f0739df7cbb86d50df90c34de8f
Adrien Leloup <contact@void.graphics>
2016-03-10 14:21:03 +0100

    Added File utils

---
+++ Utils/File.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Kabas\Utils;
+
+class File
+{
+      /**
+       * Get the contents of a json file
+       * @param  string $filePath
+       * @return object the json data
+       */
+      static function loadJson($filePath)
+      {
+            $string = file_get_contents($filePath);
+            $json = json_decode($string);
+            return $json;
+      }
+
+      /**
+       * Get directory and subdirectory structure
+       * @param  string $dirPath
+       * @return array
+       */
+      static function parseDirectory($dirPath)
+      {
+            $data = scandir($dirPath);
+            $items = [];
+
+            foreach($data as $item) {
+                  if($item !== '.' && $item !== '..') {
+                        if(is_dir($dirPath . DIRECTORY_SEPARATOR . $item)) {
+                              $items[$item] = self::parseDirectory($dirPath . DIRECTORY_SEPARATOR . $item);
+                        } else {
+                              $items[] = $item;
+                        }
+                  }
+            }
+            return $items;
+      }
+
+      /**
+       * Read all files in directory and load their content if they're json
+       * @param  string $dirPath
+       * @return array
+       */
+      static function loadJsonFromDir($dirPath)
+      {
+            $data = scandir($dirPath);
+            $items = [];
+
+            foreach($data as $item) {
+                  if($item !== '.' && $item !== '..') {
+                        if(is_dir($dirPath . DIRECTORY_SEPARATOR . $item)) {
+                              $items[$item] = self::loadJsonFromDir($dirPath . DIRECTORY_SEPARATOR . $item);
+                        } else {
+                              if(self::isJson($item)){
+                                    $items[] = self::loadJson($dirPath . DIRECTORY_SEPARATOR . $item);
+                              }
+                        }
+                  }
+            }
+            return $items;
+      }
+
+      /**
+       * Check if file has .json extension
+       * @param  string  $path
+       * @return boolean
+       */
+      static function isJson($path)
+      {
+            $path_parts = pathinfo($path);
+            if($path_parts['extension'] === 'json') return true;
+            return false;
+      }
+
+}
