01dab50b59316a6236873de0ce16518f4d14289f
Adrien Leloup <contact@void.graphics>
2016-03-14 16:07:34 +0100

    Made values optional

--- Config/Parts/Item.php
+++ Config/Parts/Item.php
@@ -6,9 +6,9 @@ class Item
 {
       public function __construct($data)
       {
-            $this->id = $data->id;
-            $this->template = $data->template;
-            $this->options = $data->options;
-            $this->data = $data->data;
+            $this->id = isset($data->id) ? $data->id : null;
+            $this->template = isset($data->template) ? $data->template : null;
+            $this->options = isset($data->options) ? $data->options : null;
+            $this->data = isset($data->data) ? $data->data : null;
       }
 }
