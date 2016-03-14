3cc789ba64ca17d951d691f3a45b1dbcb9e9fe27
Adrien Leloup <contact@void.graphics>
2016-03-14 14:55:25 +0100

    Changed theme to void

--- _bootstrap/Bootstrap/autoload-build.php
+++ _bootstrap/Bootstrap/autoload-build.php
@@ -3,7 +3,7 @@
 /* **************************************
  * AUTOLOAD BUILD
  * --------------
- * generated 14/03/2016 @ 14:54:03:31
+ * generated 14/03/2016 @ 14:55:03:09
  * In order to regenerate this file, just
  * delete it, the autoloader will
  * automatically re-build it.
@@ -11,10 +11,11 @@
  * ************************************** */
 
 return [
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/contact/about/about.class.php',
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/contact/about/about.php',
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/homepage/home.class.php',
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/homepage/homepage.php',
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/nos-services/services.class.php',
-      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Blue/pages/nos-services/services.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/contact/about/about.class.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/contact/about/about.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/homepage/home.class.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/homepage/homepage.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/nos-services/services.class.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/pages/nos-services/services.php',
+      '/Users/adrienleloup/Dev/WhiteCube/Kabas/kabas/core/../themes/Void/parts/section1.php',
 ];
