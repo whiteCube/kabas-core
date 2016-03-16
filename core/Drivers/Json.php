d5e4c6b0465fadae64ed15203c7e8839974b6a8e
Adrien Leloup <contact@void.graphics>
2016-03-16 16:57:34 +0100

    Added comments

--- core/Drivers/Json.php
+++ core/Drivers/Json.php
@@ -6,16 +6,28 @@ use \Kabas\Utils\File;
 
 class Json
 {
+      /**
+       * Loads the database configuration
+       * @return object
+       */
       public function loadDBConfig()
       {
             return File::loadJson('config/database.json');
       }
 
+      /**
+       * Loads the site configuration
+       * @return object
+       */
       public function loadSiteConfig()
       {
             return File::loadJson('config/site.json');
       }
 
+      /**
+       * Loads the social networks configuration
+       * @return object
+       */
       public function loadSocialConfig()
       {
             return File::loadJson('config/social.json');
