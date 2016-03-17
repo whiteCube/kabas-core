ce11a1f1820b73ab0ed1f59ddaedc11704122f5e
Adrien Leloup <contact@void.graphics>
2016-03-17 18:03:53 +0100

    Better lang detection

--- _bootstrap/Bootstrap/autoload-build.php
+++ _bootstrap/Bootstrap/autoload-build.php
@@ -3,7 +3,7 @@
 /* **************************************
  * AUTOLOAD BUILD
  * --------------
- * generated 17/03/2016 @ 17:57:03:27
+ * generated 17/03/2016 @ 18:02:03:50
  * In order to regenerate this file, just
  * delete it, the autoloader will
  * automatically re-build it.
