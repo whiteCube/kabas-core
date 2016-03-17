ff3c36bedafe5beb6c877719cd3d3d4622648124
Adrien Leloup <contact@void.graphics>
2016-03-17 17:58:00 +0100

    Added default lang and browser lang detection

--- core/Config/Settings/Container.php
+++ core/Config/Settings/Container.php
@@ -21,6 +21,18 @@ class Container
             $this->database = $app->driver->loadDBConfig();
             $this->site = $app->driver->loadSiteConfig();
             $this->social = $app->driver->loadSocialConfig();
+
+            $this->detectLang();
+      }
+
+      protected function detectLang()
+      {
+            $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 5);
+            if(in_array($lang, $this->site->lang->available)){
+                  $this->site->lang = $lang;
+            } else {
+                  $this->site->lang = $this->site->lang->default;
+            }
       }
 
 }
