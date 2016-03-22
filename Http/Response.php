33ff47ef150c04688202cba646d68c32f2fcbd52
Adrien Leloup <contact@void.graphics>
2016-03-22 17:42:30 +0100

    Fixed custom 404

--- Http/Response.php
+++ Http/Response.php
@@ -3,7 +3,6 @@
 namespace Kabas\Http;
 
 use Kabas\Utils\Text;
-
 use \Kabas\App;
 
 class Response
@@ -16,22 +15,20 @@ class Response
        */
       public function send($pageID)
       {
-            if($pageID === null){
+            if($pageID === null || $pageID === '404' || $pageID === '' ){
                   http_response_code(404);
                   if(array_key_exists('404', App::config()->pages->items)) {
-                        echo App::config()->pages->items['404']->data->content;
-                        return;
+                        $pageID = '404';
                   } else {
                         echo '404 par défaut';
                         return;
                   }
-            } else {
-                  $page = App::config()->pages->items[$pageID];
-                  $pageTemplate = Text::toNamespace($page->template);
-                  $themeTemplate = '\Theme\\' . App::config()->settings->site->theme .'\Pages\\' . $pageTemplate;
-                  new $themeTemplate($page);
-                  return;
             }
+            $page = App::config()->pages->items[$pageID];
+            $pageTemplate = Text::toNamespace($page->template);
+            $themeTemplate = '\Theme\\' . App::config()->settings->site->theme .'\Pages\\' . $pageTemplate;
+            new $themeTemplate($page);
+            return;
       }
 
 }
