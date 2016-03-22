6ad5373a3d27e4965bad7356f60ca250bda64612
Adrien Leloup <contact@void.graphics>
2016-03-22 10:11:04 +0100

    Small Response cleanup

--- Http/Response.php
+++ Http/Response.php
@@ -17,19 +17,18 @@ class Response
       public function send($pageID)
       {
             if($pageID === null){
+                  http_response_code(404);
                   if(array_key_exists('404', App::config()->pages->items)) {
-                        http_response_code(404);
                         echo App::config()->pages->items['404']->data->content;
                         return;
                   } else {
-                        http_response_code(404);
                         echo '404 par défaut';
                         return;
                   }
             } else {
-                  $pageTemplate = Text::toNamespace(App::config()->pages->items[$pageID]->template);
-                  $themeTemplate = '\Theme\\' . App::config()->settings->site->theme .'\Pages\\' . $pageTemplate;
                   $page = App::config()->pages->items[$pageID];
+                  $pageTemplate = Text::toNamespace($page->template);
+                  $themeTemplate = '\Theme\\' . App::config()->settings->site->theme .'\Pages\\' . $pageTemplate;
                   new $themeTemplate($page);
                   return;
             }
