c5a5d9968c4528dba719e8dbe060523308d02963
Adrien Leloup <contact@void.graphics>
2016-03-23 16:46:40 +0100

    Added comments

--- core/Config/FieldTypes/Types/Email.php
+++ core/Config/FieldTypes/Types/Email.php
@@ -6,6 +6,11 @@ class Email extends Item
 {
       public $type = "email";
 
+      /**
+       * Condition to check if the value is correct for this field type.
+       * @param  mixed $value
+       * @return bool
+       */
       public function condition($value)
       {
             return (gettype($value) === 'string' && filter_var($value, FILTER_VALIDATE_EMAIL));
