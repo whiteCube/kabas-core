66fadf593bfa6e59f34b7a76e41f166d521e4804
Adrien Leloup <contact@void.graphics>
2016-03-29 17:09:15 +0200

    Added URL FieldType methods

--- core/Config/FieldTypes/Types/Url.php
+++ core/Config/FieldTypes/Types/Url.php
@@ -6,6 +6,30 @@ class Url extends Item
 {
       public $type = "url";
 
+      public function __construct($fieldName = null, $data = null)
+      {
+            $this->fieldName = $fieldName;
+            $this->data = $data;
+
+            if(isset($data)) {
+                  $this->href = $data->href;
+                  $this->label = $data->label;
+                  $this->title = $data->title;
+            }
+
+            if(isset($this->fieldName) && isset($this->href)) {
+                  try { $this->check($fieldName, $this->href); }
+                  catch (\Kabas\Exceptions\TypeException $e) {
+                        echo $e->getMessage();
+                  }
+            }
+      }
+
+      public function __toString()
+      {
+            return $this->href;
+      }
+
       /**
        * Condition to check if the value is correct for this field type.
        * @param  mixed $value
@@ -14,6 +38,26 @@ class Url extends Item
       public function condition($value)
       {
             return filter_var($value, FILTER_VALIDATE_URL);
+      }
+
+      /**
+       * Check if url is from this domain
+       * @return boolean
+       */
+      public function isLocal()
+      {
+            $serverHost = str_replace('www.', '', $_SERVER['HTTP_HOST']);
+            $linkHost = str_replace('www.', '', $this->parse()->host);
+            return $serverHost === $linkHost;
+      }
+
+      /**
+       * Get the parsed url
+       * @return object
+       */
+      public function parse()
+      {
+            return (object) parse_url($this->href);
       }
 
 }
