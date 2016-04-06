7d8fc4e0d8d50685ddd56a167b626504c5144dac
Adrien Leloup <contact@void.graphics>
2016-04-06 14:38:16 +0200

    Version 0.1.0

--- core/App.php
+++ core/App.php
@@ -12,7 +12,7 @@ class App extends Container
       * The current Kabas version
       * @var string
       */
-      const VERSION = '0.0.4';
+      const VERSION = '0.1.0';
 
       /**
        * Activate debug mode
