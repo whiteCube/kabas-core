75eafe0b216167967d4719ff40cf1c2226a42343
Adrien Leloup <contact@void.graphics>
2016-04-12 16:51:43 +0200

    Added Date FieldType (Carbon)

---
+++ Config/FieldTypes/Types/Date.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kabas\Config\FieldTypes;
+
+use \Carbon\Carbon;
+
+class Date extends Item
+{
+      public $type = "date";
+
+      public function __construct($fieldName = null, $data = null)
+      {
+            parent::__construct($fieldName, $data);
+            $this->data = new Carbon($data);
+      }
+
+      /**
+       * Condition to check if the value is correct for this field type.
+       * @param  mixed $value
+       * @return bool
+       */
+      public function condition($value)
+      {
+            return true;
+      }
+
+      public function __get($key)
+      {
+            return $this->data->$key;
+      }
+
+      public function __set($key, $value)
+      {
+            $this->data->$key = $value;
+            return $this->data;
+      }
+
+      public function __call($method, $params)
+      {
+            return call_user_func_array([$this->data, $method], $params);
+      }
+
+      public function __toString()
+      {
+            return $this->data->__toString();
+      }
+
+}
