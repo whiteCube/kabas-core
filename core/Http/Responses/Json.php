92173abb76ef15347795188c255bcc72890bf2cd
Adrien Leloup <contact@void.graphics>
2016-04-12 14:33:07 +0200

    Added pretty option to json responses

--- core/Http/Responses/Json.php
+++ core/Http/Responses/Json.php
@@ -6,14 +6,30 @@ use Kabas\Http\Response;
 
 class Json extends Response
 {
+
+      protected $pretty = false;
+
       public function __construct($data)
       {
             $this->headers(['Content-Type: application/json']);
-            $this->data = json_encode($data);
+            $this->data = $data;
+      }
+
+      public function pretty()
+      {
+            $this->pretty = true;
+            return $this;
+      }
+
+      protected function encodeData()
+      {
+            if($this->pretty) $this->data = json_encode($this->data, JSON_PRETTY_PRINT);
+            else $this->data = json_encode($this->data);
       }
 
       public function run()
       {
+            $this->encodeData();
             $this->setHeaders();
             echo $this->data;
       }
