ad18c0f0e47cc5d1c44df256b094b8bacb99db36
Adrien Leloup <contact@void.graphics>
2016-04-13 16:03:32 +0200

    Removed $value in FieldType conditions

--- core/Config/FieldTypes/Types/Number.php
+++ core/Config/FieldTypes/Types/Number.php
@@ -11,9 +11,84 @@ class Number extends Item
        * @param  mixed $value
        * @return bool
        */
-      public function condition($value)
+      public function condition()
       {
-            return gettype($value) === 'integer';
+            return gettype($this->data) === 'integer';
+      }
+
+      public function __toString()
+      {
+            return (string) $this->modified;
+      }
+
+      public function __get($key)
+      {
+            return $this->convert($key);
+      }
+
+      public function __call($method, $args)
+      {
+            return $this->convert($method);
+      }
+
+      protected function convert($key)
+      {
+            switch (strtolower($key)) {
+                  case 'i':
+                  case 'int':
+                  case 'toint':
+                  case 'integer':
+                  case 'parseint':
+                  case 'intval':
+                        return (int) $this->getNumber();
+                        break;
+                  case 'f':
+                  case 'float':
+                  case 'tofloat':
+                  case 'parsefloat':
+                  case 'floatval':
+                        return (float) $this->getNumber();
+                        break;
+            }
+            return false;
+      }
+
+      public function add($number)
+      {
+            $number = $this->checkNumberType($number);
+            $this->modified = $this->getNumber() + $number;
+            return $this;
+      }
+
+      public function subtract($number)
+      {
+            $number = $this->checkNumberType($number);
+            $this->modified = $this->getNumber() - $number;
+            return $this;
+      }
+
+      public function divide($number)
+      {
+            $number = $this->checkNumberType($number);
+            $this->modified = $this->getNumber() / $number;
+            return $this;
+      }
+
+      public function multiply($number)
+      {
+            $number = $this->checkNumberType($number);
+            $this->modified = $this->getNumber() * $number;
+            return $this;
+      }
+
+      protected function checkNumberType($number)
+      {
+            if(is_object($number) && isset($number->data)) return $number->data;
+      }
+
+      protected function getNumber()
+      {
+            return isset($this->modified) ? $this->modified : $this->data;
       }
 
 }
