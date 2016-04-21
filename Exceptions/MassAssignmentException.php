42d806c925435ee541a1ad36ff76716608fef362
Adrien Leloup <contact@void.graphics>
2016-04-21 11:56:29 +0200

    Added exceptions

---
+++ Exceptions/MassAssignmentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kabas\Exceptions;
+
+use \Exception;
+
+class MassAssignmentException extends Exception
+{
+      public function __construct($key, $code = 0, Exception $previous = null)
+      {
+            $message = "Mass assignment exception [$key]";
+            parent::__construct($message, $code, $previous);
+      }
+}
---
+++ Exceptions/ModelNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kabas\Exceptions;
+
+use \Exception;
+
+class ModelNotFoundException extends Exception
+{
+      public function __construct($table, $code = 0, Exception $previous = null)
+      {
+            $message = "Model not found [$table]";
+            parent::__construct($message, $code, $previous);
+      }
+}
