708cd0ddcf996a8c9acdcd4e57e50c0477608a45
Toon Van den Bos <toon@whitecube.be>
2016-07-22 17:10:56 +0200

    Added automatic Partial inclusion based on Controller when content file doesnt exist

--- Content/Partials/Container.php
+++ Content/Partials/Container.php
@@ -4,6 +4,7 @@ namespace Kabas\Content\Partials;
 
 use \Kabas\App;
 use \Kabas\Utils\File;
+use \Kabas\Utils\Text;
 use \Kabas\Content\BaseContainer;
 
 class Container extends BaseContainer
@@ -20,8 +21,8 @@ class Container extends BaseContainer
        */
       public function load($part)
       {
-            $file = File::loadJson($this->getFile($part));
-            $this->items[$part] = $this->makeItem($file);
+            $item = $this->loadItem($part);
+            $this->items[$part] = $this->makeItem($item);
             return $this->get($part);
       }
 
@@ -41,7 +42,7 @@ class Container extends BaseContainer
        */
       protected function getFile($file)
       {
-            return $this->path . DS . $file . '.json';
+            return realpath($this->path . DS . $file . '.json');
       }
 
       /**
@@ -52,5 +53,44 @@ class Container extends BaseContainer
       protected function makeItem($file)
       {
             return App::getInstance()->make('\Kabas\Content\Partials\Item', [$file]);
+      }
+
+      //    TODO :
+      //    All the following should move to BaseItem
+      //    and be supported on each content type.
+
+      protected function loadItem($id)
+      {
+            $file = $this->getFile($id);
+            if($file) return $this->loadFromContent($file);
+            // The content file was not found, we'll have to check
+            // if the controller exists
+            $controller = $this->getController($id);
+            if($controller) return $this->loadFromController($id, $controller);
+      }
+
+      protected function loadFromContent($file)
+      {
+            $file = File::loadJson($file);
+            $file->controller = $this->getController($file->template);
+            return $file;
+      }
+
+      protected function loadFromController($id, $controller)
+      {
+            $file = new \stdClass();
+            $file->id = $id;
+            $file->controller = $controller;
+            $ref = new \ReflectionClass($file->controller);
+            if($ref->getStaticPropertyValue('template')){
+                  $file->template = $ref->getStaticPropertyValue('template');
+                  return $file;
+            }
+      }
+
+      protected function getController($id)
+      {
+            $c = '\Theme\\' . App::theme() .'\Partials\\' . Text::toNamespace($id);
+            if(class_exists($c)) return $c;
       }
 }
