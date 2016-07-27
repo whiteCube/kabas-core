a98f2f19ef686cc836e8bb719a036fa4c9524bc7
Toon Van den Bos <toon@whitecube.be>
2016-07-27 14:16:02 +0200

    Updated base field Item with fields restructuration

--- Fields/Item.php
+++ Fields/Item.php
@@ -6,19 +6,22 @@ class Item
 {
       public $type;
 
-      public $fieldName;
+      public $name;
 
-      public $data;
+      public $value;
 
-      public $multi = false;
+      public $output;
 
-      public function __construct($fieldName = null, $data = null, $multiVal = null)
+      public $multiple = false;
+
+      public function __construct($name = null, $value = null, $multiple = null)
       {
-            $this->fieldName = $fieldName;
-            $this->data = $data;
+            $this->name = $name;
+            $this->multiple = is_null($multiple) ? $this->multiple : $multiple;
+            $this->set($value);
 
-            if(isset($this->fieldName) && isset($this->data)) {
-                  try { $this->check($fieldName, $this->data); }
+            if(!is_null($this->name) && !is_null($this->value)) {
+                  try { $this->check(); }
                   catch (\Kabas\Exceptions\TypeException $e) {
                         echo $e->getMessage();
                   }
@@ -27,21 +30,34 @@ class Item
 
       public function __toString()
       {
-            return (string) $this->data;
+            return (string) $this->output;
       }
 
       public function __call($name, $arguments)
       {
             if(!method_exists($this, $name)) {
-                  echo '<pre>Error: Method "' . $name . '" does not exist for field type → "' . $this->type .'"</pre>';
+                  $error = 'Error: Method "' . $name . '" does not exist for field type "' . $this->type .'".';
+                  throw new \Exception($error);
             }
       }
 
+      /**
+       * Defines field's value and updates its output
+       * @param  mixed $value
+       * @return void
+       */
       public function set($value)
       {
-            $this->data = $value;
+            if($this->multiple && !is_array($value)) $value = [$value];
+            $this->value = $value;
+            $this->output = $this->parse($value);
       }
 
+      /**
+       * Generates the right form tag for this field
+       * @param  array $attributes
+       * @return string
+       */
       public function input($attributes = [])
       {
             // TODO: make this rock.
@@ -50,16 +66,39 @@ class Item
       }
 
       /**
-       * Runs the condition and throw an error if it returns false.
+       * Runs a field validation on the given/current value
+       * and throws an error if it returns false.
        * @param  string $field
        * @param  mixed $value
        * @return void
        */
-      public function check($field, $value)
+      public function check($value = null)
       {
-            if(!$this->condition()) {
-                  $error = 'Field "' . $field . '" has an incorrect value → type "' . $this->type . '".';
+            if(!$this->validate($value)) {
+                  $error = 'Field "' . $this->name . '" of type "' . $this->type . '" has an incorrect value.';
                   throw new \Kabas\Exceptions\TypeException($error);
             }
+      }
+
+      /**
+       * Validates the given/current value
+       * @param  string $field
+       * @param  mixed $value
+       * @return void
+       */
+      public function validate($value = null)
+      {
+            $value = is_null($value) ? $this->value : $value;
+            return $this->condition($value);
+      }
+
+      /**
+       * Makes an output value from given raw value
+       * @param  mixed $value
+       * @return mixed
+       */
+      protected function parse($value)
+      {
+            return $value;
       }
 }
