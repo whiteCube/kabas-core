a6eb725aa138212bc68d87b162ccf5cec89ac269
Toon Van den Bos <toon@whitecube.be>
2016-08-01 20:45:57 +0200

    Added Repeater FieldType - Added Groupable types - Updated Group FIledType since Groupable parent

--- core/Fields/Types/Repeater.php
+++ core/Fields/Types/Repeater.php
@@ -2,20 +2,47 @@
 
 namespace Kabas\Fields\Types;
 
-use \Kabas\Fields\Item;
+use \Kabas\App;
+use \Kabas\Fields\Groupable;
 
-class Repeater extends Item
+class Repeater extends Groupable
 {
       public $type = "repeater";
 
+      protected $multiple = true;
+
       /**
        * Condition to check if the value is correct for this field type.
-       * @param  mixed $value
        * @return bool
        */
       public function condition()
       {
-            return true;
+            return is_array($this->value);
+      }
+
+      /**
+       * makes options from user defined list
+       * @return array
+       */
+      protected function makeOptions($options)
+      {
+            if(!is_array($options) && !is_object($options)) throw new \Exception('Field groups require a valid fields list.');
+            return $options;
+      }
+
+      /**
+       * Makes an array of defined arrays
+       * @param  array $value
+       * @return array
+       */
+      protected function parse($value)
+      {
+            $a = [];
+            $group = App::fields()->getClass('group');
+            foreach ($value as $i => $item) {
+                  $a[] = App::getInstance()->make($group, [$i, $item, $this]);
+            }
+            return $a;
       }
 
 }
