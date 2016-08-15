004d8469e72666ef57733582c1355753048e143c
Toon Van den Bos <toon@whitecube.be>
2016-08-15 12:11:18 +0200

    Added target pages ID to acceptable values for URL Field

--- core/Fields/Types/Url.php
+++ core/Fields/Types/Url.php
@@ -2,12 +2,15 @@
 
 namespace Kabas\Fields\Types;
 
+use \Kabas\App;
 use \Kabas\Fields\Item;
 use \Kabas\Utils\Url as UrlUtil;
 
 class Url extends Item
 {
       protected $type = "url";
+
+      protected $target;
 
       /**
        * Condition to check if the value is correct for this field type.
@@ -16,7 +19,7 @@ class Url extends Item
        */
       public function condition()
       {
-            return filter_var($this->value, FILTER_VALIDATE_URL);
+            return filter_var($this->output, FILTER_VALIDATE_URL);
       }
 
       /**
@@ -25,6 +28,7 @@ class Url extends Item
        */
       public function isLocal()
       {
+            if($this->target) return true;
             if(trim(UrlUtil::parse($this->value)->base, '/') == UrlUtil::base()) return true;
             return false;
       }
@@ -35,7 +39,25 @@ class Url extends Item
        */
       public function getParsed()
       {
-            return UrlUtil::parse($this->value);
+            return UrlUtil::parse($this->output);
+      }
+
+      /**
+       * Checks if this URL has a defined internal target
+       * @return boolean
+       */
+      public function hasTarget()
+      {
+            return is_null($this->target) ? false : true;
+      }
+
+      /**
+       * Get the Url's target object if defined
+       * @return object
+       */
+      public function getTarget()
+      {
+            return $this->target;
       }
 
       /**
@@ -45,7 +67,21 @@ class Url extends Item
        */
       protected function parse($value)
       {
+            if($page = $this->findTarget($value)){
+                  $this->target = $page;
+                  return UrlUtil::to($page->id);
+            }
             return trim($value, '/');
+      }
+
+      /**
+       * Returns target page if exists
+       * @param  string $id
+       * @return object
+       */
+      protected function findTarget($id)
+      {
+            return App::content()->pages->get($id);
       }
 
 }
