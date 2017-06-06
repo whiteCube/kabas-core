beffa21725f96e375ecd509755e0022122ba2748
Adrien Leloup <contact@void.graphics>
2017-06-06 19:11:52 +0200

    100% coverage on LanguageRepository

---
+++ tests/Config/LanguageRepositoryTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Config;
+
+use Kabas\Config\LanguageRepository;
+use PHPUnit\Framework\TestCase;
+
+class LanguageRepositoryTest extends TestCase
+{
+
+	public function setUp()
+	{
+		$available = ['en-GB' => ['slug' => 'en', 'label' => 'English']];
+      	$default = 'en-GB';
+		$this->repo = new LanguageRepository($available, $default);
+	}
+
+    /** @test */
+    public function can_create_a_repository()
+    {
+    	$this->assertInstanceOf(LanguageRepository::class, $this->repo);
+    }
+
+    /** @test */
+    public function can_register_a_new_language()
+    {
+        $this->repo->register('fr-BE', ['slug' => 'fr', 'label' => 'Français']);
+        $this->assertCount(2, $this->repo->getAll());
+        $this->assertEquals('Français', $this->repo->find('fr-BE')->label);
+    }
+
+    /** @test */
+    public function can_determine_if_repository_contains_a_language()
+    {
+        $this->assertTrue($this->repo->has('en-GB'));
+        $this->assertFalse($this->repo->has('fr-CA'));
+    }
+
+    /** @test */
+    public function can_set_and_get_the_current_language()
+    {
+        $this->repo->set('en-GB');
+        $this->assertEquals('English', $this->repo->getCurrent()->native);
+    }
+
+    /** @test */
+    public function can_return_a_default_language()
+    {
+        $this->assertEquals('English', $this->repo->getDefault()->native);
+    }
+
+    /** @test */
+    public function returns_default_if_asked_unknown_language()
+    {
+        $this->assertEquals('English', $this->repo->getOrDefault('fr-FR')->native);
+    }
+
+    /** @test */
+    public function returns_null_if_no_current_language_is_set()
+    {
+        $this->assertNull($this->repo->getCurrent());
+    }
+
+    /** @test */
+    public function returns_null_if_no_default_language_is_set()
+    {
+        $available = ['en-GB' => ['slug' => 'en', 'label' => 'English']];
+        $default = '';
+        $this->repo = new LanguageRepository($available, $default);
+        $this->assertNull($this->repo->getDefault());
+    }
+
+}
