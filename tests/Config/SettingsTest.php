b91b64fed3189eb3e1cc0f96849f1d82d98b1279
Adrien Leloup <contact@void.graphics>
2017-06-07 19:32:26 +0200

    Added SettingsTest

---
+++ tests/Config/SettingsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Config;
+
+use Kabas\Config\Settings;
+use Tests\CreatesApplication;
+use PHPUnit\Framework\TestCase;
+use WhiteCube\Lingua\Service as Lingua;
+
+class SettingsTest extends TestCase
+{
+	use CreatesApplication;
+
+	protected $preserveGlobalState = false;
+    protected $runTestInSeparateProcess = true;
+
+    public function setUp()
+    {
+    	$this->app = $this->createApplication();
+    	$this->settings = $settings = new Settings;
+    }
+
+    /** @test */
+    public function can_be_properly_instanciated()
+    {
+        $this->assertInstanceOf(Settings::class, $this->settings);
+    }
+
+    /** @test */
+    public function can_retrieve_data()
+    {
+        $this->assertEquals('root', $this->settings->get('database.username'));
+    }
+
+    /** @test */
+    public function can_set_data()
+    {
+        $this->settings->set('database', ['foo' => 'bar']);
+        $this->settings->set('database.bar', ['baz']);
+        $this->settings->set('database.baz', 'test');
+        $this->assertEquals('bar', $this->settings->get('database.foo'));
+        $this->assertEquals(['baz'], $this->settings->get('database.bar'));
+        $this->assertEquals('test', $this->settings->get('database.baz'));
+    }
+
+    /** @test */
+    public function can_remove_data()
+    {
+        $this->settings->set('database', ['foo' => 'bar']);
+        $this->settings->remove('database.foo');
+        $this->assertTrue(is_null($this->settings->get('database.foo')));
+    }
+
+    /** @test */
+    public function can_pluck_data()
+    {
+        $this->settings->set('database', ['foo' => 'bar']);
+        $this->assertEquals('bar', $this->settings->pluck('database.foo'));
+        $this->assertTrue(is_null($this->settings->get('database.foo')));;
+    }
+}
