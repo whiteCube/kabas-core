f424639e58d379ce56e4a027abacff0f89be6ef1
Adrien Leloup <contact@void.graphics>
2017-06-07 19:32:44 +0200

    Added Utils/SessionTest

---
+++ tests/Utils/SessionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests;
+
+use Kabas\App;
+use Kabas\Utils\Session;
+use Tests\CreatesApplication;
+use PHPUnit\Framework\TestCase;
+
+class SessionTest extends TestCase
+{
+	use CreatesApplication;
+
+	protected $preserveGlobalState = false;
+    protected $runTestInSeparateProcess = true;
+
+    /** @test */
+    public function can_forward_method_calls_to_application_session_manager()
+    {
+    	$this->createApplication();
+    	Session::set('test', 'value');
+        $this->assertEquals('value', Session::get('test'));
+    }
+
+}
