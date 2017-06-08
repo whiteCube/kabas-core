e7f2def23d559241c1536be59280b1bbcb826742
Adrien Leloup <contact@void.graphics>
2017-06-08 11:41:32 +0200

    Removed useless files

---
+++ tests/TestTheme/public/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Kabas\App;
+
+/**
+ * Kabas application (website) launch
+ *
+ * @package  Kabas
+ * @author   WhiteCube <hello@whitecube.be>
+ */
+
+
+/*
+|--------------------------------------------------------------------------
+| Start point
+|--------------------------------------------------------------------------
+|
+| Every request is sent through this file, where the core system is
+| included and executed.
+|
+*/
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Let's fly
+|--------------------------------------------------------------------------
+|
+| Initialise a new Kabas instance and run the app.
+|
+*/
+
+$kabas = new App(realpath(__DIR__));
+$kabas->boot();
