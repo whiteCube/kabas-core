85cd19288fad1244fd52c72ae85961d9684b788f
Adrien Leloup <contact@void.graphics>
2017-06-22 11:23:02 +0200

    Added Objects\Image\ItemTest

--- src/Kabas/Objects/Image/Item.php
+++ src/Kabas/Objects/Image/Item.php
@@ -18,15 +18,13 @@ class Item
     protected $renamed;
     protected $editor;
 
-
     public function __construct($content)
     {
         if(is_array($content)) $content = (object) $content;
-        if(!is_object($content)) $this->error = true;
-        else{
-            $this->setFile(@$content->path);
-            $this->setAlt(@$content->alt);
-        }
+        if(!is_object($content)) return $this->error = true;
+        $content = $this->mergeWithBase($content);
+        $this->setFile($content->path);
+        $this->setAlt($content->alt);
     }
 
     public function __toString()
@@ -112,6 +110,17 @@ class Item
         return $this->filename . '.' . $this->extension;
     }
 
+    protected function mergeWithBase($content)
+    {
+        $base = new \stdClass;
+        $base->path = false;
+        $base->alt = false;
+        foreach($content as $key => $value) {
+            $base->$key = $value;
+        }
+        return $base;
+    }
+
     protected function makeEditor($prepareIntervention = true)
     {
         if(!$this->error) {
@@ -150,7 +159,6 @@ class Item
     protected function getAlt($string = null)
     {
         if(is_string($string)) return $string;
-        if($this->filename) return $this->filename;
-        return '';
+        return $this->filename;
     }
 }
---
+++ tests/Objects/ItemTest.php
@@ -0,0 +1,139 @@
+<?php 
+
+namespace Tests\Objects;
+
+use Kabas\Objects\Image\Item;
+use Tests\CreatesApplication;
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase 
+{
+    use CreatesApplication;
+
+    protected $preserveGlobalState = false;
+    protected $runTestInSeparateProcess = true;
+
+    public function setUp()
+    {
+        if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+        if(!defined('ROOT_PATH')) define('ROOT_PATH', __DIR__ . DS . '..' . DS . 'TestTheme');
+        if(!defined('PUBLIC_PATH')) define('PUBLIC_PATH', ROOT_PATH . DS . 'public');
+        $data = [
+            'path' => 'public/TheCapricorn/foo.jpg',
+            'alt' => 'Alt value'
+        ];
+        $this->item = new Item($data);
+    }
+
+    /** @test */
+    public function can_be_instantiated_properly()
+    {
+        $this->assertInstanceOf(Item::class, $this->item);
+    }
+
+    /** @test */
+    public function can_be_echoed()
+    {
+        $this->expectOutputString('/foo.jpg');
+        echo $this->item;
+    }
+
+    /** @test */
+    public function can_return_filesize()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class,
+        ]);
+        $this->assertSame(42950, $this->item->filesize());
+    }
+
+    /** @test */
+    public function can_return_core()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class,
+        ]);
+        $this->assertTrue(is_resource($this->item->getCore()));
+    }
+
+    /** @test */
+    public function can_return_image_dimensions()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class,
+        ]);
+        $this->assertSame(111, $this->item->height());
+        $this->assertSame(333, $this->item->width());
+    }
+
+    /** @test */
+    public function can_return_iptc_data()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class,
+        ]);
+        $this->assertSame('WhiteCube', $this->item->iptc('AuthorByline'));
+    }
+
+    /** @test */
+    public function can_return_mime_type()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class
+        ]);
+        $this->assertSame('image/jpeg', $this->item->mime());
+    }
+
+    /** @test */
+    public function can_return_the_color_of_a_pixel_within_the_image()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class
+        ]);
+        $this->assertSame('#ccd0db', $this->item->pickColor(100, 100, 'hex'));
+    }
+
+    /** @test */
+    public function can_return_alt()
+    {
+        $this->assertSame('Alt value', $this->item->alt());
+    }
+
+    /** @test */
+    public function can_use_filename_as_alt()
+    {
+        $data = ['path' => 'public/TheCapricorn/foo.jpg'];
+        $item = new Item($data);
+        $this->assertSame('foo', $item->alt());
+    }
+
+    /** @test */
+    public function can_generate_html_image_tag()
+    {
+        $this->assertSame('<img src="/foo.jpg" alt="Alt value" />', $this->item->show(false));
+    }
+
+    /** @test */
+    public function can_set_errors()
+    {
+        $data = ['path' => 'test.png'];
+        $image = new Item($data);
+        $this->assertTrue($image->error);
+        $data = ['alt' => 'No path'];
+        $image2 = new Item($data);
+        $this->assertTrue($image->error);
+    }
+
+    /** @test */
+    public function can_forward_method_calls_to_editor()
+    {
+        $this->createApplication([
+            'config' => \Kabas\Config\Container::class
+        ]);
+        $this->item->blur(2)->save();
+        $file = PUBLIC_PATH . DS . 'TheCapricorn' . DS . 'foo-blur2.jpg';
+        $this->assertTrue(file_exists($file));
+        unlink($file);
+    }
+
+}
