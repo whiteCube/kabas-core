c6235db26da1c6e98dfeb27f088e366f3b3262dc
Toon Van den Bos <toon@whitecube.be>
2017-07-04 10:45:54 +0200

    Added begin of shared files for models

--- src/Kabas/Database/ModelInterface.php
+++ src/Kabas/Database/ModelInterface.php
@@ -5,8 +5,10 @@ namespace Kabas\Database;
 interface ModelInterface
 {
     public function getObjectName() : string;
-    public function getRepository() : string;
+    public function getRepositoryName() : string;
     public function getStructurePath() : string;
+    public function getRepositoryPath($locale = null) : string;
+    public function getRepositories() : array;
     public function getRawFields();
     public function isTranslatable() : bool;
 }
