bfad2ff545de5697e777b3405e21ccf2d98b481c
Adrien Leloup <contact@void.graphics>
2017-07-05 14:49:51 +0200

    Fixed inversed DEBUG condition

--- src/Kabas/Utils/Debug.php
+++ src/Kabas/Utils/Debug.php
@@ -7,7 +7,7 @@ use Kabas\Utils\Debug\Backtrace;
 class Debug {
     public static function backtrace($echo = true, $withFunctionArguments = true)
     {
-        if(DEBUG) return false;
+        if(!DEBUG) return false;
         $items = debug_backtrace();
         array_shift($items);
         $backtrace = new Backtrace($items, $withFunctionArguments);
