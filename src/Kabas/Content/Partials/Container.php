58907e006dff7c88d39dbe30df69ff3e2a20aa62
Adrien Leloup <contact@void.graphics>
2017-07-17 14:27:58 +0200

    Replaced short variable name

--- src/Kabas/Content/Partials/Container.php
+++ src/Kabas/Content/Partials/Container.php
@@ -42,48 +42,48 @@ class Container extends BaseContainer
     //    All the following should move to BaseItem
     //    and be supported on each content type.
 
-    protected function loadItem($id)
+    protected function loadItem($identifier)
     {
         // check if controller exists
-        $controller = $this->getController($id);
-        if($controller) return $this->loadFromController($id, $controller);
+        $controller = $this->getController($identifier);
+        if($controller) return $this->loadFromController($identifier, $controller);
         // Controller does not exist.
         // check view file
-        $view = $this->getView($id);
-        if($view) return $this->loadFromView($id, $view);
+        $view = $this->getView($identifier);
+        if($view) return $this->loadFromView($identifier);
         // Not found.
-        throw new NotFoundException($id, 'partial');
+        throw new NotFoundException($identifier, 'partial');
     }
 
-    protected function loadFromController($id, $controller)
+    protected function loadFromController($identifier, $controller)
     {
         $file = new \stdClass();
-        $file->id = $id;
+        $file->id = $identifier;
         $file->controller = $controller;
         $ref = new \ReflectionClass($file->controller);
-        if(!$ref->getStaticPropertyValue('template')) throw new NotFoundException($id,'partial');
+        if(!$ref->getStaticPropertyValue('template')) throw new NotFoundException($identifier,'partial');
         $file->template = $ref->getStaticPropertyValue('template');
         return $file;
     }
 
-    protected function loadFromView($id, $view)
+    protected function loadFromView($identifier)
     {
         $file = new \stdClass();
-        $file->id = $id;
+        $file->id = $identifier;
         $file->controller = null;
-        $file->template = $id;
+        $file->template = $identifier;
         return $file;
     }
 
-    protected function getController($id)
+    protected function getController($identifier)
     {
-        $controller = '\\Theme\\' . App::themes()->getCurrent('name') .'\\Partials\\' . Text::toNamespace($id);
+        $controller = '\\Theme\\' . App::themes()->getCurrent('name') .'\\Partials\\' . Text::toNamespace($identifier);
         if(class_exists($controller)) return $controller;
     }
 
-    protected function getView($id)
+    protected function getView($identifier)
     {
-        $file = THEME_PATH . DS . 'views' . DS . 'partials' . DS . $id . '.php';
+        $file = THEME_PATH . DS . 'views' . DS . 'partials' . DS . $identifier . '.php';
         return realpath($file);
     }
 }
