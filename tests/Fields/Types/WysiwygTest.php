9df0bb5b74ddf5e8f7ed0c427d8efed835ca2945
Toon Van den Bos <toon@whitecube.be>
2017-08-30 11:18:03 +0200

    Added wysiwyg table test

--- tests/Fields/Types/WysiwygTest.php
+++ tests/Fields/Types/WysiwygTest.php
@@ -15,7 +15,6 @@ class WysiwygTest extends TestCase
         $data->label = "Main text";
         $data->type = "wysiwyg";
         $this->textfield = new Wysiwyg('title', $data);
-        $this->textfield->set('<h1>My foo title</h1>');
     }
 
     /** @test */
@@ -27,8 +26,16 @@ class WysiwygTest extends TestCase
     /** @test */
     public function can_format_headings()
     {
+        $this->textfield->set('# My foo title');
         $this->textfield->headingLevel(3);
         $this->assertSame('<h3>My foo title</h3>', $this->textfield->get());
+    }
+
+    /** @test */
+    public function can_convert_inline_table()
+    {
+        $this->textfield->set("| Foo | Bar |\n\r| ---- | ---- |\n\r| Test | Test |");
+        $this->assertContains('<table>', $this->textfield->get());
     }
 
 }
