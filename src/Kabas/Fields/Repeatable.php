152f3d43fc1f05ecc22262fdd3d3ed272e9897a2
Adrien Leloup <contact@void.graphics>
2017-10-02 15:48:45 +0200

    Added take() method to repeatable fields

--- src/Kabas/Fields/Repeatable.php
+++ src/Kabas/Fields/Repeatable.php
@@ -28,6 +28,15 @@ class Repeatable extends Aggregate
     }
 
     /**
+     * Take only a portion of this field's value
+     * @return array
+     */
+    public function take($amount)
+    {
+        return array_slice($this->output, 0, $amount);
+    }
+
+    /**
      * makes an unique field name for repeatable fields
      * @param  int $i
      * @return string
