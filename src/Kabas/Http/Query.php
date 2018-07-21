9dd1ffd44f3a8976c99b22f18358cd13b57a6f5a
Toon Van den Bos <toon@whitecube.be>
2018-07-21 16:36:25 +0200

    Added docblocks to Http\Query class

--- src/Kabas/Http/Query.php
+++ src/Kabas/Http/Query.php
@@ -54,21 +54,41 @@ class Query
         $this->setURI($uri);
     }
 
+    /**
+     * Define protocol type
+     * @param bool $secure 
+     * @return void
+     */
     public function setSSL($secure)
     {
         $this->scheme = $secure ? 'https' : 'http';
     }
 
+    /**
+     * Define host string
+     * @param string $host 
+     * @return void
+     */
     public function setHost($host)
     {
         $this->host = rtrim($host, '/');
     }
 
+    /**
+     * Define sub-directory string
+     * @param ?string $script 
+     * @return void
+     */
     public function setRoot($script = null)
     {
         $this->root = is_null($script) ? null : $this->extractRootFromScript($script);
     }
 
+    /**
+     * Define full URI string, locale, and requested route
+     * @param string $uri 
+     * @return void
+     */
     public function setURI($uri)
     {
         $this->uri = trim($this->extractRealURIFromURI($uri), '/');
@@ -76,6 +96,11 @@ class Query
         $this->route = $this->extractRouteFromRealURI($this->uri, $this->locale);
     }
 
+    /**
+     * Retrieves the sub-directory to Kabas' index file
+     * @param string $script 
+     * @return string|null
+     */
     protected function extractRootFromScript($script)
     {
         preg_match('/(.+)?index.php$/', $script, $matches);
@@ -84,6 +109,11 @@ class Query
         return $root;
     }
 
+    /**
+     * Substracts sub-directory from given URI
+     * @param string $uri 
+     * @return string
+     */
     protected function extractRealURIFromURI($uri)
     {
         $uri = trim(explode('?', $uri)[0], '/');
@@ -93,6 +123,11 @@ class Query
         return $uri;
     }
 
+    /**
+     * Retrieves locale string in given URI
+     * @param string $uri 
+     * @return string|null
+     */
     protected function extractLocaleFromRealURI($uri)
     {
         preg_match('/^\/?([^\/]+)?/', $uri, $matches);
@@ -102,6 +137,12 @@ class Query
         return $matches[1];
     }
 
+    /**
+     * Substracts locale from given URI and formats remaing
+     * string in order to make it machable for defined routes
+     * @param string $uri 
+     * @return string
+     */
     protected function extractRouteFromRealURI($uri, $locale = null)
     {
         if(!$locale) return '/' . $uri;
